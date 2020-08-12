1e054090fb636e73956cc882fc2ed752db57308a
kim-dev <lunatic7208@gmail.com>
2020-08-12 17:28:46 +0900

    update:회원가입발리데이션체크진행중

--- sample-project/src/app/login/sign-up-dialog/sign-up-dialog.component.ts
+++ sample-project/src/app/login/sign-up-dialog/sign-up-dialog.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
 import { MatStepper } from '@angular/material/stepper';
+import { } from '@angular/material/datepicker';
 @Component({
   selector: 'app-sign-up-dialog',
   templateUrl: './sign-up-dialog.component.html',
@@ -9,28 +10,44 @@ import { MatStepper } from '@angular/material/stepper';
 export class SignUpDialogComponent implements OnInit {
   firstFormGroup: FormGroup;
   secondFormGroup: FormGroup;
+  // tslint:disable:typedef
   constructor(
-    // tslint:disable-next-line:variable-name
+    // tslint:disable:variable-name
     private _formBuilder: FormBuilder
-  ) { }
+  ) {
+   }
 
   ngOnInit(): void {
     this.firstFormGroup = this._formBuilder.group({
       name: ['', Validators.required],
-      birthDay: ['', Validators.required],
-      phoneNumber: ['', Validators.required]
+      birthDay: ['', [Validators.required]],
+      phoneNumber: ['', [Validators.required, Validators.pattern('^\\d{11}$')]]
     });
+
     this.secondFormGroup = this._formBuilder.group({
       id: ['', Validators.required],
-      password: ['', Validators.required],
+      password: ['', Validators.required, Validators.pattern('')],
       name: ['', Validators.required],
       birthDay: ['', Validators.required],
-      phoneNumber: ['', Validators.required]
+      phoneNumber: ['', [Validators.required, Validators.pattern('^\\d{11}$')]]
     });
   }
-  // tslint:disable-next-line:typedef
+
+  regVaildation(key: string, control: AbstractControl) {
+    let reg = '';
+    switch (key) {
+      case 'password': {
+        reg = '';
+        if (!control.value.match(reg)) {
+          return { pattenError: true  };
+        }
+        break;
+      }
+    }
+    return null;
+  }
   signUp(level: number, stepper: MatStepper) {
-    switch (level){
+    switch (level) {
       case 1: {
         stepper.next();
         break;
